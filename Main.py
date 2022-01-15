de612e9fee422601039a06694651048e5503b9a3
Neekow <45294301+Neek0w@users.noreply.github.com>
2022-01-15 23:07:07 +0100

    Création des fonctions pour bouton tkinter Traduction fichier csv 2019

--- Main.py
+++ Main.py
@@ -1,17 +1,19 @@
-import numpy as np            # Data manipulation
 import pandas as pd           # Dataframe manipulation
 from matplotlib import pyplot as plt # Data visualization
 import seaborn as sns         # Data visualization
-import scipy.stats as stats
-import chart_studio.plotly as py # visualization library
-from plotly.offline import init_notebook_mode # plotly offline mode
 from plotly.offline import iplot
 import plotly.graph_objs as go # plotly graphical object
 import warnings
 from plotly.offline import plot
 warnings.filterwarnings('ignore')
 
+# Lecture du fichier avec le module pandas
+
 data2019 = pd.read_csv("dataSet/2019.csv") # Lire le dataset
+
+# CREATION DE TOUTES LES FONCTIONS
+
+# Création du tableau de corrélation
 
 def tableau_corr():
     f, ax = plt.subplots(figsize=(10, 10))
@@ -22,12 +24,13 @@ def tableau_corr():
                 center=0,
                 fmt=".1f",
                 square=True
-               )
+                )
+
+# création de la carte
 
 def map ():
     location = pd.read_csv("dataSet/concap.csv") #dataset capitales
-    data_new = pd.merge(location[['CountryName', 'CapitalName', 'CapitalLatitude', 'CapitalLongitude']], \
-    data2019, left_on='CountryName', right_on='Country or region')
+    data_new = pd.merge(location[['CountryName', 'CapitalName', 'CapitalLatitude', 'CapitalLongitude']], data2019, left_on='CountryName', right_on='Pays')
     happiness_score = data_new['Score'].astype(float)
     data = [dict(
         type='choropleth',
@@ -35,7 +38,7 @@ def map ():
         locations=data_new['CountryName'],
         z=happiness_score,
         locationmode='country names',
-        text=data_new['Country or region'],
+        text=data_new['Pays'],
         colorbar=dict(
             title='Score du Bonheur',
             titlefont=dict(size=25),
@@ -54,4 +57,182 @@ def map ():
     iplot(choromap, validate=False)
     plot(choromap)
 
-map()
+# Partie sur le Score en général
+
+def score_all():
+    plt.figure(figsize=(10, 25))
+    sns.barplot(x=data2019['Score'], y=data2019['Pays'])
+    plt.xlabel('Score')
+    plt.ylabel('Pays')
+    plt.title('Score du bonheur')
+
+def score_10er():
+    sns.barplot(x=data2019['Score'], y=data2019['Pays'][:10])
+    plt.xlabel('Score')
+    plt.ylabel('Pays')
+    plt.title('Score du bonheur 10 premiers pays')
+    plt.show()
+
+def score_10la():
+    sns.barplot(x=data2019['Score'], y=data2019['Pays'][-10:])
+    plt.xlabel('Score')
+    plt.ylabel('Pays')
+    plt.title('Score du bonheur 10 derniers pays')
+    plt.show()
+
+# Partie sur le PIB par habitant
+
+economie2019 = data2019.sort_values(by=['PIB par habitant'], ascending = False)
+
+def PIB_all():
+    plt.figure(figsize=(10, 25))
+    sns.barplot(x=economie2019['PIB par habitant'], y=economie2019['Pays'])
+    plt.xlabel('PIB par habitant')
+    plt.ylabel('Pays')
+    plt.title('Classement PIB par habitant par pays')
+    plt.show()
+
+def PIB_10er():
+    sns.barplot(x=economie2019['PIB par habitant'], y=economie2019['Pays'][:10])
+    plt.xlabel('PIB par habitant')
+    plt.ylabel('Pays')
+    plt.title('Classement PIB par habitant des 10 premiers pays')
+    plt.show()
+
+def PIB_10la():
+    sns.barplot(x=economie2019['PIB par habitant'], y=economie2019['Pays'][-10:])
+    plt.xlabel('PIB par habitant')
+    plt.ylabel('Pays')
+    plt.title('Classement PIB par habitant des 10 derniers pays')
+    plt.show()
+
+# Esperance de Vie
+
+life2019 = data2019.sort_values(by=['Esperance de vie'], ascending = False)
+
+def vie_all():
+    plt.figure(figsize=(10, 25))
+    sns.barplot(x=life2019['Esperance de vie'], y=life2019['Pays'])
+    plt.xlabel('Esperance de vie en bonne santé')
+    plt.ylabel('Pays')
+    plt.title('Classement esperance de vie par pays')
+    plt.show()
+
+def vie_10er():
+    sns.barplot(x=life2019['Esperance de vie'], y=life2019['Pays'][:10])
+    plt.xlabel('Esperance de vie en bonne santé')
+    plt.ylabel('Pays')
+    plt.title('Classement esperance de vie des 10 premiers pays')
+    plt.show()
+
+def vie_10la():
+    sns.barplot(x=life2019['Esperance de vie'], y=life2019['Pays'][-10:])
+    plt.xlabel('Esperance de vie en bonne santé')
+    plt.ylabel('Pays')
+    plt.title('Classement esperance de vie des 10 derniers pays')
+    plt.show()
+
+# Partie sur la generosite
+
+gene2019 = data2019.sort_values(by=['Generosite'], ascending = False)
+
+def gene_all():
+    sns.barplot(x=gene2019['Generosite'], y=gene2019['Pays'])
+    plt.xlabel('Generosite')
+    plt.ylabel('Pays')
+    plt.title('Classement Generosité 10 premiers pays')
+    plt.show()
+
+def gene_10er():
+    sns.barplot(x=gene2019['Generosite'], y=gene2019['Pays'][:10])
+    plt.xlabel('Generosite')
+    plt.ylabel('Pays')
+    plt.title('Classement Generosité 10 derniers pays')
+    plt.show()
+
+def gene_10la():
+    sns.barplot(x=gene2019['Generosite'], y=gene2019['Pays'][-10:])
+    plt.xlabel('Generosite')
+    plt.ylabel('Pays')
+    plt.title('Classement Generosité 10 derniers pays')
+    plt.show()
+
+# Partie sur la corruption
+
+corr2019 = data2019.sort_values(by=['Corruption'], ascending = False)
+
+def corr_all():
+    plt.figure(figsize=(10, 25))
+    sns.barplot(x=corr2019['Corruption'], y=corr2019['Pays'])
+    plt.xlabel('Perception de la corruption')
+    plt.ylabel('Pays')
+    plt.title('Classement perception de la corruption par pays')
+    plt.show()
+
+def corr_10er():
+    sns.barplot(x=corr2019['Corruption'], y=corr2019['Pays'][:10])
+    plt.xlabel('Perception de la corruption')
+    plt.ylabel('Pays')
+    plt.title('Classement perception 10 premiers par pays')
+    plt.show()
+
+def corr_10la():
+    sns.barplot(x=corr2019['Corruption'], y=corr2019['Pays'][-10:])
+    plt.xlabel('Perception de la corruption')
+    plt.ylabel('Pays')
+    plt.title('Classement perception de la corruption par pays')
+    plt.show()
+
+# Partie sur la Famille
+
+supp2019 = data2019.sort_values(by=['Famille'], ascending = False)
+
+def fam_all():
+    plt.figure(figsize=(10, 25))
+    sns.barplot(x=supp2019['Famille'], y=supp2019['Pays'])
+    plt.xlabel('Support social')
+    plt.ylabel('Pays')
+    plt.title('Classement du support social par pays')
+    plt.show()
+
+def fam_10er():
+    sns.barplot(x=supp2019['Famille'], y=supp2019['Pays'][:10])
+    plt.xlabel('Support social')
+    plt.ylabel('Pays')
+    plt.title('Classement du support social des 10 premiers pays')
+    plt.show()
+
+def fam_10la():
+    sns.barplot(x=supp2019['Famille'], y=supp2019['Pays'][-10:])
+    plt.xlabel('Support social')
+    plt.ylabel('Pays')
+    plt.title('Classement du support social des 10 derniers pays')
+    plt.show()
+
+# Partie sur la liberte
+
+free2019 = data2019.sort_values(by=['Liberte'], ascending = False)
+
+def lib_all():
+    plt.figure(figsize=(10, 25))
+    sns.barplot(x=free2019['Liberte'], y=free2019['Pays'])
+    plt.xlabel('liberté de faire des choix')
+    plt.ylabel('Pays')
+    plt.title('Classement de la liberté par pays')
+    plt.show()
+
+def lib_10er():
+    sns.barplot(x=free2019['Liberte'], y=free2019['Pays'][:10])
+    plt.xlabel('liberté de faire des choix')
+    plt.ylabel('Pays')
+    plt.title('Classement de la liberté des 10 premiers pays')
+    plt.show()
+
+def lib_10la():
+    sns.barplot(x=free2019['Liberte'], y=free2019['Pays'][-10:])
+    plt.xlabel('liberté de faire des choix')
+    plt.ylabel('Pays')
+    plt.title('Classement de la liberté des 10 derniers pays')
+    plt.show()
+
+vie_10er()
